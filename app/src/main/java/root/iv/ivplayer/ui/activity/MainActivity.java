20c68e27d33803748bdb2e26d5c714bfa60222ba
lichtstrahl <cool.rainbow2012@yandex.ru>
2020-05-11 22:42:17 +0300

    firebase-auth-ui

--- app/src/main/java/root/iv/ivplayer/ui/activity/MainActivity.java
+++ app/src/main/java/root/iv/ivplayer/ui/activity/MainActivity.java
@@ -1,15 +1,23 @@
 package root.iv.ivplayer.ui.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
 
 import root.iv.ivplayer.R;
 import root.iv.ivplayer.ui.fragment.GameFragment;
@@ -30,13 +38,37 @@ public class MainActivity extends AppCompatActivity implements
             .fragment(LoginFragment.getInstance())
             .tag("fragment:login")
             .build();
+    private static final int RC_SIGN_IN = 101;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
-        setFragment();
+//        setFragment();
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build(),
+                new AuthUI.IdpConfig.PhoneBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build()
+        );
+
+        Intent intent = AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(providers).build();
+        startActivityForResult(intent, RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                authSuccessful(FirebaseAuth.getInstance().getCurrentUser());
+            } else {
+                Toast.makeText(this, "Неудачный вход", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
     private void setFragment() {
