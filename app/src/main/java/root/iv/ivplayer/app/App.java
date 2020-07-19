542aed92dd4117687ee513826037008d27a69c03
lichtstrahl <cool.rainbow2012@yandex.ru>
2020-07-19 13:52:22 +0300

    Добавил инициализацию логов Timber

--- app/src/main/java/root/iv/ivplayer/app/App.java
+++ app/src/main/java/root/iv/ivplayer/app/App.java
@@ -2,20 +2,19 @@ package root.iv.ivplayer.app;
 
 import android.app.Application;
 
-import okhttp3.OkHttpClient;
-import root.iv.ivplayer.network.http.RoomAPI;
-import root.iv.ivplayer.network.http.UserAPI;
+import timber.log.Timber;
 
 public class App extends Application {
     private static final String TAG = "tag:ws";
-    private static final String API = "/api";
-    private static final String API_USERS = "/api/users";
-    private static final String API_ROOMS = "/api/rooms";
-    private static UserAPI userAPI;
-    private static RoomAPI roomAPI;
-    private static OkHttpClient httpClient;
 
     public static String getTag() {
         return TAG;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Timber.plant(new Timber.DebugTree());
     }
 }
